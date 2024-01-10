905ac7b03505bf2bf006a5e6b0546d27065e4d8f
dimicw <104204285+dimicw@users.noreply.github.com>
2024-01-10 23:48:15 +0100

    add delay

--- request.js
+++
@@ -1,7 +0,0 @@
-Notification.requestPermission().then(function(permission) {
-    if (permission === 'granted') {
-      console.log('Permission for push notifications granted');
-    } else {
-      console.error('Permission for push notifications denied');
-    }
-  });
--- subscribe.js
+++
@@ -1,9 +0,0 @@
-navigator.serviceWorker.ready.then(function(registration) {
-    registration.pushManager.subscribe({ userVisibleOnly: true })
-      .then(function(subscription) {
-        // Send subscription data to your server
-      })
-      .catch(function(error) {
-        console.error('Error subscribing to push notifications:', error);
-      });
-  });
